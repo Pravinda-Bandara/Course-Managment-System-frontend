c91edda2c72bfd5d0c2a311c85bec2c99bb900de
Pravinda-Bandara <bandarapravinda@gmail.com>
2024-06-13 12:34:58 +0530

    implement custom input field

---
+++ src/components/InputFieldComponent.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const InputFieldComponent = ({ type, placeholder, value, onChange }) => {
+    return (
+        <input
+            type={type}
+            placeholder={placeholder}
+            value={value}
+            onChange={onChange}
+            className="custom-input"
+        />
+    );
+};
+
+export default InputFieldComponent;
